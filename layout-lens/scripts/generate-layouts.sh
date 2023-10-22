2fe9cc1e8390ffd3afe0d07d48d3a9a10fc5abd9
Matei Adriel <rafaeladriel11@gmail.com>
2023-10-22 17:24:28 +0200

    Fix tiny comment

--- layout-lens/scripts/generate-layouts.sh
+++ layout-lens/scripts/generate-layouts.sh
@@ -3,7 +3,7 @@ shopt -s globstar  # Enable the globstar option to use **
 for config in **/lens.json; do
   # Check if the path is a regular file
   if [ -f "$config" ]; then 
-    # Replace the .txt extension with a new extension (e.g., .newext)
+    # Replace the .json extension with a new extension .svg
     out="${config%.json}.svg"
     # Generate preview
     layout-lens "$config" "$out"
